49930cb706f8556a3c6c238d972a76b430d82086
Maciej Czarnecki <mcczarny@gmail.com>
2024-12-05 10:17:53 +0100

    Support expiration option (#9)

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
  * A github action that uploads an image and stores its url.
  */
 
-const core = require('@actions/core')
-const uploadImage = require('./uploadImage')
-const assert = require('assert')
+const core = require('@actions/core');
+const uploadImage = require('./uploadImage');
+const assert = require('assert');
 
 /**
  * Runs the action to upload an image.
@@ -14,40 +14,48 @@ const assert = require('assert')
  *  apiKey - The API key if the upload method requires it.
  * Output:
  *  url - The url of the image or undefined if the upload failed.
+ *  expiration - The expiration of the image or undefined if the upload method does not support it or failed.
  *
  * most @actions toolkit packages have async methods
  */
 async function run() {
   try {
-    const paths = core.getMultilineInput('path')
-    const uploadMethod = core.getInput('uploadMethod')
-    const apiKey = core.getInput('apiKey')
-    assert(paths.length > 0, 'Missing mandatory parameter "paths"')
+    const paths = core.getMultilineInput('path');
+    const uploadMethod = core.getInput('uploadMethod');
+    const apiKey = core.getInput('apiKey');
+    assert(paths.length > 0, 'Missing mandatory parameter "paths"');
 
-    const results = new Map()
+    const results = new Map();
     await Promise.all(
-      paths.map(async (pathToUpload) => {
-        core.info(`Uploding an image ${pathToUpload} to ${uploadMethod}...`)
+        paths.map(async (pathToUpload) => {
+          core.info(`Uploding an image ${pathToUpload} to ${uploadMethod}...`);
 
-        const resultUrl = await uploadImage(pathToUpload, uploadMethod, apiKey)
-        assert(resultUrl, 'There was an error uploading the image.')
-        core.info('inner result:' + resultUrl)
-        results.set(pathToUpload, resultUrl)
-      }),
-    )
+          const result = await uploadImage(pathToUpload, uploadMethod, apiKey);
+          assert(result, 'There was an error uploading the image.');
+          core.info('inner result:' + result);
+          results.set(pathToUpload, result);
+        }),
+    );
 
-    core.info('results: ' + [...results.entries()])
+    core.info('results: ' + [...results.entries()]);
     const url = paths
-      .map((pathToUpload) => {
-        return results.get(pathToUpload)
-      })
-      .join('\n')
-    core.info('final url:' + url)
+        .map((pathToUpload) => {
+          return results.get(pathToUpload)?.url;
+        })
+        .join('\n');
+    core.info('final url:' + url);
 
-    core.setOutput('url', url)
+    core.setOutput('url', url);
+
+    const expiration = paths
+        .map((pathToUpload) => {
+          return results.get(pathToUpload)?.expiration;
+        })
+        .join('\n');
+    core.setOutput('expiration', expiration);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
--- test/uploadImage.test.js
+++ test/uploadImage.test.js
@@ -1,6 +1,9 @@
 const uploadImage = require('../uploadImage');
 const process = require('process');
 const assert = require('assert');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
 
 /**
  * Tests for uploadImage.
@@ -9,23 +12,25 @@ const assert = require('assert');
  */
 
 test('upload an image', async () => {
-  const url = await uploadImage(
+  const result = await uploadImage(
       'test-resources/0.png',
       'imgbb',
       process.env['API_KEY'],
   );
+  const url = result.url;
   expect(url).toMatch(new RegExp('https:\\/\\/i.ibb.co\\/.*\\.png'));
+  const expiration = result.expiration;
+  expect(expiration).toBe(0);
 });
 
 test('upload with a wrong API key, should return undefined', async () => {
-  const url = await uploadImage(
+  const result = await uploadImage(
       'test-resources/0.png',
       'imgbb',
       'incorrect-api-key',
   );
-
   assert(
-      typeof url === 'undefined',
+      typeof result === 'undefined',
       'An incorrect api-key didn\'t generate an undefined value.',
   );
 });
@@ -47,4 +52,69 @@ test('upload with an incorrect path, should throw an exception', async () => {
             'imgbb',
             process.env['API_KEY']),
   ).rejects.toThrow();
+});
+
+describe('Test expiration option', () => {
+  let uniqueImagePath;
+
+  // Generates a random gif file to be sure that expiration option is set by test.
+  // If we try to reupload the same image to imgbb, it won't take into account
+  // the expiration option and return settings of the original upload.
+  // Check ./gif_data_description.txt to get more information about the gif format.
+  beforeEach(() => {
+    // Data before global color table.
+    const hexPrefix = "47494638396101000100800000";
+    // Data after global color table.
+    const hexSuffix = "21f90401000000002c000000000100010000020144003b";
+    // 6 bytes with global color table. We use it to generate an unique image.
+    const randomSixBytes = [...Array(6)]
+        .map(() => Math.floor(Math.random() * 256)
+          .toString(16)
+          .padStart(2, '0'))
+        .join('');
+    const imageData = Buffer.from(hexPrefix + randomSixBytes + hexSuffix, 'hex');
+    uniqueImagePath = path.join(__dirname, 'image.gif');
+    fs.writeFileSync(uniqueImagePath, imageData);
+  });
+
+  afterEach(() =>{
+    fs.unlinkSync(uniqueImagePath);
+  })
+
+  const ExpirationInSeconds = 60;
+  const LongTestTimeout = ExpirationInSeconds * 1000 * 10;
+  // Long test to for expiration option.
+  // The minimum expiration time for imgbb is 1 minute.
+  test('upload an image with expiration option,', async () => {
+    const result = await uploadImage(
+        uniqueImagePath,
+        'imgbb',
+        process.env['API_KEY'],
+        {expiration: ExpirationInSeconds},
+    );
+    const url = result.url;
+    expect(url).toMatch(new RegExp('https:\\/\\/i.ibb.co\\/.*\\.gif'));
+    expect(result.expiration).toBe(ExpirationInSeconds);
+
+    // Verify  that the image is accessible.
+    const response = await fetch(url);
+    expect(response.status).toBe(200);
+      const startTime = Date.now();
+      // We cannot guarantee that the image will expire after given time, but
+      // we can at least verify that it expires after a reasonable amount of time.
+      const maxWaitTime = 9 * ExpirationInSeconds * 1000; // 9 * expiration time
+      let response2;
+
+      // Actively wait for the image to expire, checking every 10 seconds
+      while (true) {
+        response2 = await fetch(url);
+        if (response2.status === 404) {
+          break;
+        }
+        if (Date.now() - startTime > maxWaitTime) {
+          assert.fail('Image did not expire after 9 minutes');
+        }
+        await new Promise((resolve) => setTimeout(resolve, 10000)); // Wait 10 seconds
+      }
+  }, LongTestTimeout);
 });
